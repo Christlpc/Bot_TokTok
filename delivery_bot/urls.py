b03c5b15cf66ff6edd593bdc9a5dba1904c14fe9
christlpc <pierrechristivie.landzi@esprit.tn>
2025-09-05 16:14:02 +0200

    fix allowed hosts for render-5

--- delivery_bot/urls.py
+++ delivery_bot/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from chatbot.views import whatsapp_webhook
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chatbot/', include('chatbot.urls')),
+    path('webhook/', whatsapp_webhook, name='whatsapp_webhook'),
+
 ]
